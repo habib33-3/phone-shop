ae5088af863141aefc055700092eca8f5e77d784
Habibur Rahman <habib.swi16@gmail.com>
2023-09-23 13:01:15 +0600

    project setup

---
+++ src/Router/Route.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const Route = createBrowserRouter([
+  {
+    path: "/",
+    element: <p className="text-center text-4xl">Home</p>,
+  },
+]);
+
+export default Route;
